74654055eb74a0768a66f56aed8274d51548864c
NotRx <216323458+NotReallyRx@users.noreply.github.com>
2026-09-10 18:17:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { server as wisp } from "@mercuryworkshop/wisp-js/server";
 
 const PORT = Number(process.env.PORT || 10000);
 const server = http.createServer((req, res) => {
-    if (req.url === "/" || req.url === "/health") {
+    if (req.url === "/" || req.url === "/h") {
         res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
         res.end(JSON.stringify({ status: "ok" }));
         return;
